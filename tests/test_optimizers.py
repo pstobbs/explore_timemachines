c4b0145c9ef9f0a3f5a1d77bc9e8f4110c1c9cc3
pstobbs <pstobbs@yahoo.com>
2022-09-06 16:43:29 +0100

    initial setup

---
+++ tests/test_optimizers.py
@@ -0,0 +1,19 @@
+from timemachines.skatertools.tuning.hyper import using_humpday
+if using_humpday:
+    from humpday.optimizers.alloptimizers import OPTIMIZERS
+    from humpday.objectives.classic import CLASSIC_OBJECTIVES
+    import random
+
+    # Just to check if someone (ahem) breaks humpday
+
+    def test_optimizers():
+        n_trials = 10
+        n_dim = random.choice([2,3])
+        optimizer = random.choice(OPTIMIZERS)
+        objective = random.choice(CLASSIC_OBJECTIVES)
+        best_val, best_x, feval_count = optimizer(objective, n_trials=n_trials,n_dim=n_dim,with_count=True)
+
+
+if __name__=='__main__':
+    assert using_humpday,'pip install humpday'
+    test_optimizers()
